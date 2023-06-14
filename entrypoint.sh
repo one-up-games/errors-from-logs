f5519088566ed4c5c50277d10aa1883e57671dfa
Maxim Matveev <maxim.w.matveev@gmail.com>
2023-06-14 10:26:29 +0300

    checj result

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 result=$(grep "error cs" ${INPUT_LOGFILE})
 
 status=$?
-if [$status -eq 0 ]; then
-echo "Build errors: $result" >> $GITHUB_STEP_SUMMARY
+if [ "$status" -eq 0 ]; then
+	echo "Build errors: $result" >> $GITHUB_STEP_SUMMARY
 fi
 
 echo "$result" >> $GITHUB_STEP_SUMMARY
