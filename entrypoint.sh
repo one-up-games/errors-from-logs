3ea46652a3a9cba0c09de350a5089157282494da
Maxim Matveev <maxim.w.matveev@gmail.com>
2023-06-14 14:28:33 +0300

    GGG-522 Вывести в workflow Summary ошибки сборки - Continue check errors step

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ result=$(grep "error" ${INPUT_LOGFILE})
 status=$?
 
 if [ $status -eq 0 ]; then
+	echo "=====ERRORS=====" >> "$GITHUB_STEP_SUMMARY"
 	for item in $result
 	do
 		echo -e "$item" >> "$GITHUB_STEP_SUMMARY"
