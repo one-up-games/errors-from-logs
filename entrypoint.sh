15a00eb42031a2978c987b8e7006e82be897a28b
Maxim Matveev <maxim.w.matveev@gmail.com>
2023-06-14 12:59:28 +0300

    no message

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo ${INPUT_LOGFILE}
+result=$(grep "error" ${INPUT_LOGFILE})
 
-echo grep "error cs" ${INPUT_LOGFILE})
+echo $result
+
+echo "result=$result" >> $GITHUB_OUTPUT
 
 exit 0
