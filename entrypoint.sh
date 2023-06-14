83545b0b49ebb631a3ba8ec57578ff26cf326976
Maxim Matveev <maxim.w.matveev@gmail.com>
2023-06-14 13:50:33 +0300

    no message

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,9 @@ IFS=$'\n'
 
 result=$(grep "error CS" ${INPUT_LOGFILE})
 
-echo -e $result
-
 for item in $result
 do
-	echo -e "result=$item" >> "$GITHUB_STEP_SUMMARY"
+	echo -e "$item" >> "$GITHUB_STEP_SUMMARY"
 done
 
 
